e788f44ddf8ce9a9ee5d82676eabec9d6da69aa0
dbijaya <dbijaythapa@gmail.com>
2021-04-22 12:21:58 +0545

    [ADD] database migration files.

---
+++ migrations/db_init.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE book(
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    author VARCHAR NOT NULL,
+    publication VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+    );
